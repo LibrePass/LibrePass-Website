0d5e8201e8e1cc440467731edee1e5db6666b323
M3DZIK <me@medzik.dev>
2023-09-10 14:37:31 +0200

    Initial commit
    Signed-off-by: M3DZIK <me@medzik.dev>

---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+    import Timer from "svelte-material-icons/TimerOutline.svelte"
+    import ShieldCheck from "svelte-material-icons/ShieldCheckOutline.svelte"
+    import Devices from "svelte-material-icons/Devices.svelte"
+
+    import phoneScreenshot from "$lib/images/screenshots/phone.avif"
+</script>
+
+<svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="container min-h-screen flex justify-center items-center max-w-[980px] mx-auto gap-5">
+    <div class="flex flex-col justify-start gap-4">
+        <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold">
+            Take control of your passwords.
+        </h1>
+
+        <p class="max-w-[720px] text-lg sm:text-xl">
+            With LibrePass, convenience and security go hand in hand. Through LibrePass, your passwords are secure while
+            you have easy access to them.
+        </p>
+
+        <div class="flex">
+            <a href="/auth/register" class="flex btn btn-primary">
+                Get Started &rarr;
+            </a>
+        </div>
+    </div>
+
+    <div class="hidden lg:block">
+        <div class="mockup-phone">
+            <div class="camera"></div>
+            <div class="display w-[250px]">
+                <img src={phoneScreenshot} alt="screenshot of android application" />
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="flex min-h-[50vh] w-full bg-primary text-primary-content">
+    <div class="container flex justify-center items-center mx-auto">
+        <div class="flex flex-wrap justify-center text-center gap-12 p-20">
+            <div class="badge-item">
+                <Timer class="h-16 w-16 p-2 outline rounded-xl" />
+
+                <p class="font-bold">
+                    Fast
+                </p>
+
+                <p>Instant access to your passwords</p>
+            </div>
+
+            <div class="badge-item">
+                <ShieldCheck class="h-16 w-16 p-2 outline rounded-xl" />
+
+                <p class="font-bold">
+                    Secure
+                </p>
+
+                <p>Your passwords are end-to-end encrypted and securely stored</p>
+            </div>
+
+            <div class="badge-item">
+                <Devices class="h-16 w-16 p-2 outline rounded-xl" />
+
+                <p class="font-bold">
+                    Accessible
+                </p>
+
+                <p>Available on all your favorite platforms</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<style lang="postcss">
+    .badge-item {
+        @apply flex flex-col gap-2 items-center max-w-[16em];
+    }
+</style>
