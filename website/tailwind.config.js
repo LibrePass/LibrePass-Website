4cccc7556bc662a84dcbc1524995217654a9279f
M3DZIK <me@medzik.dev>
2023-09-16 20:53:57 +0200

    Update color palette
    Signed-off-by: M3DZIK <me@medzik.dev>

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -9,22 +9,22 @@ export default {
         themes: [
             {
                 light: {
-                    primary: '#9a4500',
-                    secondary: '#fdb58c',
-                    accent: '#9a4500',
-                    neutral: '#2b3440',
-                    'base-100': '#ffffff',
+                    primary: '#fdb58c',
+                    // secondary: '#9a4500',
+                    // accent: '#9a4500',
+                    // neutral: '#2b3440',
+                    'base-100': '#fffbff',
                     info: '#3abff8',
                     success: '#36d399',
                     warning: '#fbbd23',
                     error: '#f87272'
                 },
                 dark: {
-                    primary: '#fdb58c',
-                    secondary: '#9a4500',
-                    accent: '#fdb58c',
-                    neutral: '#e5e7eb',
-                    'base-100': '#1e1e1e',
+                    primary: '#9a4500',
+                    // secondary: '#9a4500',
+                    // accent: '#fdb58c',
+                    // neutral: '#e5e7eb',
+                    'base-100': '#201a17',
                     info: '#3abff8',
                     success: '#36d399',
                     warning: '#fbbd23',
