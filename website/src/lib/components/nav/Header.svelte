74883f813b480e1eec89791b93c1fb5b99f72583
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-09-17 15:30:30 +0200

    Update header menu on mobile devvices
    Signed-off-by: M3DZIK <me@medzik.dev>

--- website/src/lib/components/nav/Header.svelte
+++ website/src/lib/components/nav/Header.svelte
@@ -44,7 +44,7 @@
     }
 </script>
 
-<div class="drawer">
+<!-- <div class="drawer">
     <input id="drawer" type="checkbox" class="drawer-toggle" />
 
     <div class="drawer-content">
@@ -96,4 +96,45 @@
             {/each}
         </ul>
     </div>
+</div> -->
+
+<div class="sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all
+            duration-100 bg-base-100 text-base-content">
+    <nav class="navbar w-full">
+        <div class="navbar-start">
+            <div class="dropdown">
+                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                <!-- svelte-ignore a11y-label-has-associated-control -->
+                <label tabindex="0" class="btn btn-ghost lg:hidden">
+                    <Menu class="h-5 w-5" />
+                </label>
+                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    {#each sites as site}
+                        <li><a href={site.href}>{site.title}</a></li>
+                    {/each}
+                </ul>
+            </div>
+
+            <div class="flex items-center">
+                <a href="/" class="flex-0 btn normal-case btn-ghost px-2">
+                    <LibrePassIcon class="h-6 w-6 md:h-7 md:w-7" />
+                    <div class="text-lg inline-flex md:text-2xl">
+                        <span>Libre</span>
+                        <span class="text-primary">Pass</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="navbar-center"></div>
+
+        <div class="navbar-end">
+            <ul class="menu menu-horizontal min-h-full bg-base-100 invisible md:visible">
+                {#each sites as site}
+                    <li><a href={site.href}>{site.title}</a></li>
+                {/each}
+            </ul>
+        </div>
+    </nav>
 </div>
