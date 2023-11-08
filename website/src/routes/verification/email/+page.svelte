c228ac4c547215fb8a380ee0c99f4ebd63ac8648
M3DZIK <me@medzik.dev>
2023-11-08 22:31:39 +0100

    Add page displayed after successful verification of email address

---
+++ website/src/routes/verification/email/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Seo from '$lib/components/Seo.svelte';
+</script>
+
+<Seo title="Email address verification" />
+
+<section class="container flex h-90-header justify-center items-center max-w-[980px] mx-auto gap-5">
+    <div class="flex flex-col justify-start gap-4 text-center sm:text-start">
+        <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold">Email address has been verified!</h1>
+
+        <div class="flex justify-center">
+            <a href="/auth/login" class="flex btn btn-success"> Login &rarr; </a>
+        </div>
+    </div>
+</section>
