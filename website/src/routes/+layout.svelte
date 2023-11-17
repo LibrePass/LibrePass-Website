12ffcaabb5ff5bfc1b927307da8479ec4eb0c9de
M3DZIK <me@medzik.dev>
2023-11-17 18:29:12 +0100

    Add page load progress bar

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,8 +1,25 @@
 <script>
+    import NProgress from 'nprogress';
+
     import Header from '$lib/components/nav/Header.svelte';
     import Toast from '$lib/components/utilities/Toast/Toast.svelte';
+    import { navigating } from '$app/stores';
 
+    import 'nprogress/nprogress.css';
     import './../app.postcss';
+
+    NProgress.configure({
+        minimum: 0.16
+    });
+
+    $: {
+        if ($navigating) {
+            NProgress.start();
+        }
+        if (!$navigating) {
+            NProgress.done();
+        }
+    }
 </script>
 
 <Toast />
