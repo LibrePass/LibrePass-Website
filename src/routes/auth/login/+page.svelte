e679cc0b07c5a3418bd6fadb0c7544091d33f570
M3DZIK <me@medzik.dev>
2024-03-26 18:28:42 +0100

    Do not show auth form in card on mobile

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -102,7 +102,7 @@
 </script>
 
 <section class="h-full-header flex sm:justify-center sm:items-center">
-    <div class="card p-6 space-y-4 sm:max-w-sm">
+    <div class="p-6 space-y-4 sm:card sm:max-w-sm">
         <h1 class="text-xl text-center">
             {$_('login.title')}
         </h1>
